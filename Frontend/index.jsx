549a1ab0ab7e40665a3917bdc68b5f234ba34072
nishu-02 <0264nishu@gamil.com>
2025-01-01 12:13:40 +0530

    Changed the structure and the entry point

---
+++ Frontend/index.jsx
@@ -0,0 +1,5 @@
+import { registerRootComponent } from 'expo';
+import App from './App';  // Import the main App component
+
+// Automatically registers the component for Expo
+registerRootComponent(App);

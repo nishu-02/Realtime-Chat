3571859bd80ec5fdc3e70883f24bc6a3371e20f2
nishu-02 <0264nishu@gamil.com>
2025-01-01 15:54:23 +0530

    Setting up the Navigation

--- Frontend/App.jsx
+++ Frontend/App.jsx
@@ -1,12 +1,30 @@
 import React from 'react';
-import { Text, View, StyleSheet } from 'react-native';
+import { Text, View, StyleSheet, StatusBar } from 'react-native';
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 import SplashScreen from './screens/Splash';
-const App = () => {
+import SignIn from './screens/SignIn';
+import SignUp from './screens/SignUp';
+import Home from './screens/Home';
+import MessageScreen from './screens/MessageScreen';
+import SearchScreen from './screens/SearchScreen';
+
+const stack = createNativeStackNavigator();
+function App() {
   return (
-    <SplashScreen>
-      
-    </SplashScreen>
+    <NavigationContainer>
+      <StatusBar barStyle={dark-content} />
+      <Stack.Navigatior>
+        <Stack.Screen name="Splash" component={SplashScreen} />
+        <Stack.Screen name="SignIn" component={SignIn} />
+        <Stack.Screen name="SignUp" component={SignUp} />
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="SearchScreen" component={SearchScreen} />
+        <Stack.Screen name="MessageScreen" component={MessageScreen} />        
+      </Stack.Navigatior>
+    </NavigationContainer>
   );
 };
 
---
+++ Frontend/screens/Message.jsx
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+function MessageScreen() {
+  return (
+    <View>
+      <Text>MessageScreen</Text>
+    </View>
+  )
+}
+export default MessageScreen;

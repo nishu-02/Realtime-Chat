67947c91e2d51a46425e703482472bae85c719a5
Nishant Garg <161738527+nishu-02@users.noreply.github.com>
2025-02-08 21:57:11 +0530

    Update asgi.py

--- Core/backend/asgi.py
+++ Core/backend/asgi.py
@@ -1,17 +1,17 @@
 import os
-import django 
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
+
+import django
+django.setup()  # Ensure Django is initialized before anything else
+
 from django.core.asgi import get_asgi_application
 from django_channels_jwt_auth_middleware.auth import JWTAuthMiddlewareStack
 from channels.security.websocket import AllowedHostsOriginValidator
 from channels.routing import ProtocolTypeRouter, URLRouter
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
-
-django.setup()
+import main.routing
 
 django_asgi_app = get_asgi_application()
-
-import main.routing
 
 application = ProtocolTypeRouter({
     'http': django_asgi_app,
